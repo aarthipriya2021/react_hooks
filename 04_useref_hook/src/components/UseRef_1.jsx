92cf79190a0b8997ac104d34f1aef50401228ed6
Aarthipriya A <a.aarthipriya2021@gmail.com>
2023-10-29 16:21:55 +0530

    added comments

--- 04_useref_hook/src/components/UseRef_1.jsx
+++ 04_useref_hook/src/components/UseRef_1.jsx
@@ -1,14 +1,20 @@
+// first step importing the useRef() hook
 import React, { useState, useRef } from "react";
 
 const Reference = () => {
+
+  // for state management, useing the useState() hook
   const [count, setCount] = useState(0);
   const [text, setText] = useState("");
 
+  // second step the declaring the variable to useRef() for each element which we want
   const inputOneRef = useRef();
   const inputTwoRef = useRef();
 
+  // manipulating or changing dom element using useRef() hook.
   const btnHandlerOne = () => {
     console.log(inputOneRef);
+    // current value of that ref
     inputOneRef.current.style.backgroundColor = "Green";
     inputOneRef.current.style.color = "white";
   };
@@ -20,6 +26,8 @@ const Reference = () => {
   };
   return (
     <div>
+      {/* adding the reference for dom element in corresponding elemented */}
+      {/* We do not manipulate or change the ref in return  */}
       <h2>useRef Hook</h2>
       <input
         type="number"
@@ -27,6 +35,7 @@ const Reference = () => {
         onChange={(e) => {
           setCount(e.target.value);
         }}
+        // adding ref attribute to dom element
         ref={inputOneRef}
       />
 
