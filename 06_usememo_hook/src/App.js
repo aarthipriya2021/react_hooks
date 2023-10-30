188462e2a90973f7e8a435a8c553c7687b81b3da
Aarthipriya A <a.aarthipriya2021@gmail.com>
2023-10-30 19:12:37 +0530

    added useMemo() hook

---
+++ 06_usememo_hook/src/App.js
@@ -0,0 +1,13 @@
+import Counter from './components/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>UseMemo Hook</h2>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
